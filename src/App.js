2f26de1e3736416190e50b4011acf76c4cee8afc
Conqueror2000 <pratikvadher2000@gmail.com>
2023-09-14 11:22:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import EmployeeForm from './components/EmployeeForm';
-import TechnicianForm from './components/TechnicianForm';
-import LoginForm from './components/LoginForm';
-=======
 import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
@@ -17,29 +10,10 @@ import TechnicianForm from './components/TechnicianForm';
 import LoginForm from './components/LoginForm';
 import EmployeeDashboard from './components/EmployeeDashboard';
 import TechnicianDashboard from './components/TechnicianDashboard';
->>>>>>> 7dda517 (Dashboard)
 import './App.css';
 
 function App() {
   const [isEmployee, setIsEmployee] = useState(true);
-<<<<<<< HEAD
-
-  return (
-    <Router>
-      <div className='App container'>
-        <h1 className='text-center my-5'>{window.location.pathname === '/' ? 'Web Ticketing App' : 'Register'}</h1>
-        <Routes>
-          <Route path='/register' element={isEmployee ? <EmployeeForm /> : <TechnicianForm />} />
-          <Route path='/' element={<LoginForm />} />
-        </Routes>
-        <div className='text-center mt-3'>
-        {window.location.pathname === '/register' ?
-          <button onClick={() => setIsEmployee(!isEmployee)} className='btn btn-primary'>
-            {isEmployee ? 'Switch to Technician' : 'Switch to Employee'}
-          </button>: void 0}
-        </div>
-      </div>
-=======
   const location = useLocation();
 
   useEffect(() => {
@@ -97,68 +71,8 @@ function AppWrapper() {
   return (
     <Router>
       <App />
->>>>>>> 7dda517 (Dashboard)
     </Router>
   );
 }
 
-<<<<<<< HEAD
-export default App;
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import EmployeeForm from './components/EmployeeForm';
-// import TechnicianForm from './components/TechnicianForm';
-// import LoginForm from './components/LoginForm';
-// import './App.css';
-
-// function App() {
-//   let [IsEmployee, SetEmployee] = useState(true);
-//   function ChangeEmployee() {
-//     SetEmployee((Employee) => !Employee);
-//   }
-//   let [IsRegister, SetRegister] = useState(true);
-//   function ChangeRegister() {
-//     SetRegister((r) => !r);
-//   }
-//   return (
-//     <div className='App container'>
-//       <h1 className='text-center my-5'>Web Ticketing App</h1>
-//       {IsRegister ? (
-//         <LoginForm />
-//       ) : IsEmployee ? (
-//         <EmployeeForm />
-//       ) : (
-//         <TechnicianForm />
-//       )}
-//       {IsRegister ? (
-//         <button className='btn btn-primary' onClick={ChangeRegister}>
-//           Register
-//         </button>
-//       ) : (
-//         <button className='btn btn-primary' onClick={ChangeRegister}>
-//           Login
-//         </button>
-//       )}
-//       {IsEmployee ? (
-//         <button className='btn btn-primary' onClick={ChangeEmployee}>
-//           Technician
-//         </button>
-//       ) : (
-//         <button className='btn btn-primary' onClick={ChangeEmployee}>
-//           Employee
-//         </button>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
-=======
 export default AppWrapper;
->>>>>>> 7dda517 (Dashboard)
